aa9d2f30067e172292f203f9f065956c6f1882b1
yuchengzhang24 <98350905+yuchengzhang24@users.noreply.github.com>
2024-10-15 13:38:35 -0600

    Switching machines

--- HW2 Doubly Linked List/List.cpp
+++ HW2 Doubly Linked List/List.cpp
@@ -9,8 +9,8 @@
 #include "List.h"
 using namespace std;
 
-void List::insert(int value)
-{}
+void List::insert(int value){
+}
 
 void List::insertAtIndex(int data, int index)
 {}
@@ -19,7 +19,18 @@ void List::deleteAtIndex(int index)
 void List::readItem(int index){}
 void List::reverseList(){}
 
-void List::printForward()
-{}
+void List::printForward(){
+  //Creates a current node pointer that points to the next of head pointer.
+  Node *currentNode = headPtr -> getNextPtr();
+
+  //Creates a while loop that traverses through the list until the next of current node points to NULL (end of list is reached).
+  while (currentNode != NULL){
+    //Gets the data at current node and prints it out, then sets current node to the next of current. 
+    cout << currentNode -> getData() << ", ";
+    currentNode = currentNode -> getNextPtr();
+  }
+  cout << endl;
+}
+
 void List::printBackward()
 {}
